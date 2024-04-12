7b6134c6fdc79d87fa8f7c4c9b0f9604a3e39249
Caliza <rances.rodriguez@gmail.com>
2024-04-11 17:09:01 -0700

    added shadows and <p> tags to card

--- pet-feeder/src/Pannel.js
+++ pet-feeder/src/Pannel.js
@@ -3,9 +3,15 @@ import React from 'react';
 function Pannel({}) {
 
     return (
+        
         <div className='card'>
             <h2 className='card-header'>Pet Feeder</h2>
+            <div>
+            <p className='controls'>Set Time</p>
+            <p className='controls'>Set Time</p>
+            </div>
         </div>
+        
     )
 }
 
