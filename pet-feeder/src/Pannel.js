78ce565552ca7e94c4e7ad2a2af052ff6306777a
Caliza <rances.rodriguez@gmail.com>
2024-04-28 10:46:20 -0700

    added "set feed" buttons

--- pet-feeder/src/Pannel.js
+++ pet-feeder/src/Pannel.js
@@ -7,12 +7,12 @@ function Pannel({}) {
         <div className='card'>
             <h2 className='card-header'>Pet Feeder</h2>
             <div className='control'>
-            <label className='controls'>Set Time</label>
-            <label className='controls'>Set Time</label>
-            <label className='controls'>Set Time</label>
-            <label className='controls'>Set Time</label>
-            <label className='controls'>Set Time</label>
-            <label className='controls'>Set Time</label>
+            <label className='controls'>1st Feed</label>
+            <label className='controls'>2nd Feed</label>
+            <label className='controls'>3rd Feed</label>
+            <label className='controls'>4th Feed</label>
+            <label className='controls'>5th Feed</label>
+            <label className='controls'>6th Feed</label>
             </div>
             <div>            
             <input className='time-input' type="time" id="appt"  /> 
@@ -21,6 +21,14 @@ function Pannel({}) {
             <input className='time-input'type="time" id="appt"  />  
             <input className='time-input'type="time" id="appt"  />  
             <input className='time-input'type="time" id="appt"  />    
+            </div>
+            <div>
+                <button>Set Feed</button>
+                <button>Set Feed</button>
+                <button>Set Feed</button>
+                <button>Set Feed</button>
+                <button>Set Feed</button>
+                <button>Set Feed</button>
             </div>
         </div>
         
