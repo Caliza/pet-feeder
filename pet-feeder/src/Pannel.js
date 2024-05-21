548c8fc7ccfe4573e74ae951b2f1e11590f7a9cb
Caliza <rances.rodriguez@gmail.com>
2024-05-21 15:19:40 -0700

    deleted extra divs and buttons

--- pet-feeder/src/Pannel.js
+++ pet-feeder/src/Pannel.js
@@ -20,28 +20,11 @@ function handleSubmit(e) {
         
         <div className='card'>
             <h2 className='card-header'>Pet Feeder</h2>
-            <div className='control'>
-            <label className='controls'>1st Feed</label>
-            <label className='controls'>2nd Feed</label>
-            <label className='controls'>3rd Feed</label>
-            <label className='controls'>4th Feed</label>
-            <label className='controls'>5th Feed</label>
-            <label className='controls'>6th Feed</label>
-            </div>
+            
             <div>            
-            <input className='time-input' value={newItem} onChange={e => setNewItem(e.target.value)} type="time" id="appt"  /> 
-            <input className='time-input'type="time" id="appt"  />  
-            <input className='time-input'type="time" id="appt"  />  
-            <input className='time-input'type="time" id="appt"  />  
-            <input className='time-input'type="time" id="appt"  />  
-            <input className='time-input'type="time" id="appt"  />    
+            <input className='time-input' value={newItem} onChange={e => setNewItem(e.target.value)} type="time" id="appt"  />  
             </div>
             <div>
-                <button className='feed-button'>Set Feed</button>
-                <button className='feed-button'>Set Feed</button>
-                <button className='feed-button'>Set Feed</button>
-                <button className='feed-button'>Set Feed</button>
-                <button className='feed-button'>Set Feed</button>
                 <button className='feed-button'>Set Feed</button>
             </div>
         </div>
