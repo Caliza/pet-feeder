f1bd7cbcb386067bb18b0f2ee514b7ad85db75a3
Caliza <rances.rodriguez@gmail.com>
2024-05-26 14:06:07 -0700

    added a shortcircuit

--- pet-feeder/src/Pannel.js
+++ pet-feeder/src/Pannel.js
@@ -44,6 +44,7 @@ function deleteTodo(id) {
                 <button className='feed-button'>Set Feed</button>
             </div>
         <ul className="list">
+            {todos.length === 0 && "No Todos"}
             {todos.map(todo => {
                 return (<li key={todo.id}>
                     <label>
