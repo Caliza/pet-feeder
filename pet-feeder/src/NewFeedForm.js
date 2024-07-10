28b0e885e7cdcc4ec2712adcb00b6d8dbdd625c1
Caliza <rances.rodriguez@gmail.com>
2024-07-10 14:36:40 -0700

    redacted NewFeedForm.js

--- pet-feeder/src/NewFeedForm.js
+++ pet-feeder/src/NewFeedForm.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export function NewFeedForm({ onSubmit}) {
+export function NewFeedForm({ onSubmit }) {
     const [newItem, setNewItem] = useState("")
 
     function handleSubmit(e) {
@@ -24,7 +24,7 @@ export function NewFeedForm({ onSubmit}) {
                 id="item"
                 />
             </div>
-            <button>Set Time</button>
+            <button className="btn">Set Time</button>
         </form>
     )
 }
