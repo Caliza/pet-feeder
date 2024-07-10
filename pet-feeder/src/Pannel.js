0ecf4ffd825fe1682f7c65c00331e654d0e25b51
Caliza <rances.rodriguez@gmail.com>
2024-07-10 14:22:35 -0700

    redacted Pannel.js

--- pet-feeder/src/Pannel.js
+++ pet-feeder/src/Pannel.js
@@ -1,70 +1,51 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 import { NewFeedForm } from './NewFeedForm';
+import { FeedList } from './FeedList';
 
-function Pannel({}) {
-const [newItem, setNewItem] = useState("") 
-const [todos, setTodos] =useState([]) 
+function Pannel() {
+    const [todos, setTodos] = useState(() => {
+        const localValue = localStorage.getItem("ITEMS")
+        if (localValue == null) return []
 
-function handleSubmit(e) {
-    e.preventDefault()
-
-    setTodos(currentTodos => {
-        return [
-            ...currentTodos,
-            { id: crypto.randomUUID(), title: newItem, completed: false},
-        ]
+        return JSON.parse(localValue)
     })
 
-    setNewItem("")
-}
-function toggleTodo(id, completed) {
-    setTodos(currentTodos => {
-        return currentTodos.map(todo => {
-            if (todo.id === id) {
-                return { ...todo, completed}
-            }
-            return todo
+    useEffect(() => {
+        localStorage.setItem("ITEMS", JSON.stringify(todos))
+    }, [todos])
+
+    function addTodo(title) {
+        setTodos(currentTodos => {
+            return [
+                ...currentTodos,
+                { id: crypto.randomUUID(), title, completed: false },
+            ]
         })
-    })
-}
-function deleteTodo(id) {
-    setTodos(currentTodos => {
-        return currentTodos.filter(todo => todo.id !== id)
-    })
-}
+    }
+
+    function toggleTodo(id, completed) {
+        setTodos(currentTodos => {
+            return currentTodos.map(todo => {
+                if (todo.id === id) {
+                    return { ...todo, completed }
+                }
+                return todo
+            })
+        })
+    }
+
+    function deleteTodo(id) {
+        setTodos(currentTodos => {
+            return currentTodos.filter(todo => todo.id !== id)
+        })
+    }
 
     return (
         <>
-        <div></div>
-        <div className='card'>
-            <h2 className='card-header'><NewFeedForm/></h2>
-            
-            <div>            
-            <input className='time-input' value={newItem} onChange={e => setNewItem(e.target.value)} type="time" id="appt"  />  
-            </div>
-            
-        <ul className="list">
-            {todos.length === 0 && "No Todos"}
-            {todos.map(todo => {
-                return (<li key={todo.id}>
-                    <label>
-                        <input 
-                        type="checkbox" 
-                        checked={todo.completed}
-                        onChange={e => toggleTodo(todo.id, e.target.checked)}
-                        />
-                        {todo.title}
-                    </label>
-                    <button 
-                        onClick={() =>deleteTodo(todo.id)}className="btn btn-danger"
-                    >
-                        Delete
-                    </button>
-                </li>
-                )
-            })}
-        </ul>    
-        </div>
+            <NewFeedForm onSubit={addTodo} />
+            <h1 className='header'>Feed Times</h1>
+            <FeedList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
         </>
     )
 }
